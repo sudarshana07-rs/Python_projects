eab64c115daf2f2c06dddfd99b90fafdf2c79f11
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-06-16 20:30:48 +0530

    commit

--- Period_tracker.py
+++ Period_tracker.py
@@ -3,3 +3,11 @@ print("Your Cycle, Your Health ,Your Balance ❤️❤️❤️!!! ")
 name=input("Enter your dear name Girlie: ")
 print(f'{name} ❤️, Welcome to Period Calender!!!')
 age=int(input("Enter your age: "))
+first_time=input("Is it your first time getting period: ")
+if (first_time== 'Yes'or "yes" or 'Yes'):
+    print(f"{name}Congratulations On your first period💕💕💕")
+start_date=input("Enter the start date of last period: ")
+avg_cycle=input("Enter your average cycle length: ")
+period_duration=input("Enter your period duration: ")
+next_period= start_date + avg_cycle   # Next Period
+ 
