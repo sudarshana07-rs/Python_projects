a80b90ba7cda7c380846229f48dd0dac59d1e9ea
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-07-10 20:47:05 +0530

    commit

--- to_do_list.py
+++ to_do_list.py
@@ -1,8 +1,20 @@
 print(f'To Do List !!!')
 list=[]
+list_completed=[]
 list_name=input("Enter the List name: ")
 list_no=int(input("Enter the total number of tasks for today : "))
 for i in range(list_no):
     list_task=input(f'Enter the {i+1} task: ')
-    final_list=list.append(list_task)
-print(f'Final List: {final_list}')
+    list.append(list_task)
+# print(f'Final List: {list}')
+print("Final List 🎯: ")
+for i in list:
+    print(i)
+for i in range(list_no):
+    completed_task=input("Enter the completed Task: ")
+    if (completed_task in list):
+        list.remove(completed_task)
+        print('Upadated List: ')
+        for i in list:
+            print(i)
+
