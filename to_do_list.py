004d3406a57b92d13b311f6f324af4e805d3a55d
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-07-10 20:23:46 +0530

    commit

--- to_do_list.py
+++ to_do_list.py
@@ -1,1 +1,8 @@
 print(f'To Do List !!!')
+list=[]
+list_name=input("Enter the List name: ")
+list_no=int(input("Enter the total number of tasks for today : "))
+for i in range(list_no):
+    list_task=input(f'Enter the {i+1} task: ')
+    final_list=list.append(list_task)
+print(f'Final List: {final_list}')
